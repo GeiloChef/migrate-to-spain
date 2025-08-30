789241bb4a49735a44966fff9106f1cc87271deb
Felix Dziatkowski <felix.dziatkowski@vc-trade.de>
2025-08-30 15:04:14 +0200

    Add tailwind css support

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,8 @@
 <template>
+  <UApp>
   <div>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
   </div>
+  </UApp>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,18 @@
+import { fileURLToPath } from 'node:url'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-
+  alias: {
+    '@': fileURLToPath(new URL('./', import.meta.url)),
+    '~': fileURLToPath(new URL('./', import.meta.url)),
+  },
   modules: [
     '@nuxt/content',
     '@nuxt/eslint',
     '@nuxt/image',
     '@nuxt/ui'
-  ]
+  ],
+  css: ['~/assets/css/main.css']
 })
